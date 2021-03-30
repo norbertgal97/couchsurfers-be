91937355dbefde8154b15a9a1aa3b7917ef20fc5
Gál Norbert <norbert.gal97@gmail.com>
2021-03-30 21:57:16 +0200

    Create DTOs for requests, create login, sign up, logout endpoints, encrypt pwd

---
+++ src/main/java/com/norbertgal/couchsurfersbe/services/authentication/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.norbertgal.couchsurfersbe.services.authentication;
+
+import com.norbertgal.couchsurfersbe.domain.User;
+import com.norbertgal.couchsurfersbe.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findByEmail(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        return new UserDetailsImpl(user);
+    }
+}
