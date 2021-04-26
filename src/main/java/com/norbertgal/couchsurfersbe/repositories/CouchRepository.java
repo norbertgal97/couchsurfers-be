b7de8c6a3b649c72ccdedeff2f23a3ab2c964e38
Gál Norbert <norbert.gal97@gmail.com>
2021-04-26 12:12:07 +0200

    Add @Repository annotations

--- src/main/java/com/norbertgal/couchsurfersbe/repositories/CouchRepository.java
+++ src/main/java/com/norbertgal/couchsurfersbe/repositories/CouchRepository.java
@@ -2,6 +2,8 @@ package com.norbertgal.couchsurfersbe.repositories;
 
 import com.norbertgal.couchsurfersbe.domain.Couch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CouchRepository extends JpaRepository<Couch, Long> {
 }
--- src/main/java/com/norbertgal/couchsurfersbe/repositories/ReservationRepository.java
+++ src/main/java/com/norbertgal/couchsurfersbe/repositories/ReservationRepository.java
@@ -2,10 +2,12 @@ package com.norbertgal.couchsurfersbe.repositories;
 
 import com.norbertgal.couchsurfersbe.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     List<Reservation> findByUserId(Long userId);
